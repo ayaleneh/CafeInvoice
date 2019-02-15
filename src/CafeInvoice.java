19f1f8a5cd3cbe40f0a6eba310fbaf6e0d4fd88a
ayaleneh <getaunayaleneh@gmail.com>
2019-02-15 05:49:37 -0500

    Initial commit

---
+++ src/CafeInvoice.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class CafeInvoice {
+    static ArrayList<String> name=new ArrayList<>();
+    static ArrayList<String> address=new ArrayList<>();
+    static ArrayList<String> date=new ArrayList<>();
+    static ArrayList<String> accountNumber=new ArrayList<>();
+    static ArrayList<String> itemName=new ArrayList<>();
+    static ArrayList<Double> price=new ArrayList<>();
+    static ArrayList<Integer> quantity=new ArrayList<>();
+    static ArrayList<Boolean>taxiable=new ArrayList<>();
+    static ArrayList<String> state=new ArrayList<>();
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Cafe_Controller.java
@@ -0,0 +1,24 @@
+public class Cafe_Controller {
+    //create while loop to control weather the user want to add more item or not as we did...for the privious project
+    //like
+    public static void accepter(){
+        while(true){
+            //accept input from user here
+        }
+    }
+    public static  void displayCustomer(){
+        //used to display customer detail like
+        //name,address,Date,Account number
+        //you display like
+        //for(int i=0;i<CafeInvoice.name.size();i++ ){
+        //you display detail here
+        // }
+    }
+    public static void displayItem(){
+     //used to display item detail like
+        //item name,quantity,price,Cost
+        //for(int i=0;i<CafeInvoice.itemName.size();i++ ){
+        //you display detail here
+        // }
+    }
+}
